37d3f66f9188889e5cbb0b45aa67cbca41614169
Darialr <darial.ryaguzova@gmail.com>
2024-02-29 17:03:17 +0700

    Ready

--- 14.py
+++ 14.py
@@ -1,4 +1,4 @@
 n = float(input())
 hours = int(n/30)
 minutes = int((n % 30)/0.5)
-print(f'С начала суток прошло: {hours}ч. {minutes}мин.')
+print(f'С начала суток прошло: {hours} ч. {minutes} мин.')
